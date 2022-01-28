fee1b38fdb668cb63990c61f957f5ecde4795682
SakthiGitAdd <sakthi36ksr@gmail.com>
2022-01-28 14:19:05 +0530

    Taskone

---
+++ src/org/text/Text.java
@@ -0,0 +1,34 @@
+package org.text;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Text {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramu\\eclipse-workspace\\DayFour\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+//		driver.get("http://www.greenstech.in/selenium-course-content.html");
+//		WebElement textvalue = driver.findElement(By.xpath("//h1[text()='Selenium Certification Training in Chennai']"));
+//		String text = textvalue.getText();
+//	System.out.println(text);
+//	WebElement containstext = driver.findElement(By.xpath("//p[contains(text(),'        Learn')] "));
+//	String text2 = containstext.getText();
+//	System.out.println(text2);
+//	
+	
+	
+		driver.get("https://www.facebook.com/");
+		WebElement textuser = driver.findElement(By.id("email"));
+	textuser.sendKeys("sakthi123@gmail.com");
+	String attribute = textuser.getAttribute("value");
+	System.out.println(attribute);
+	String attribute2 = textuser.getAttribute("id");
+	System.out.println(attribute2);
+	
+	
+	
+	}
+
+}
